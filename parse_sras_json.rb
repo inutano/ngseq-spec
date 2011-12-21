f247358230feafd3e21501968fd8678e2b5d738c
iNut <iNut@tkrtng.(none)>
2011-12-21 18:48:31 +0900

    refactoring parse_sras_json.rb

--- parse_sras_json.rb
+++ parse_sras_json.rb
@@ -5,10 +5,23 @@ require "json"
 require "open-uri"
 require "pp"
 
-class parse_sras_json
-	def initialize
-		@wholeset = 
+class SRAsJSONParser
+	def initialize(json)
+		@wholeset = open(json){|f| JSON.load(f)}["ResultSet"]["Result"]
+	end
+	
+	def all_subid
+		@wholeset.map{|set| set["sra_id"]}
+	end
+	
+	def journal_info(subid)
+		@wholeset.select{|s| s["sra_id"] == subid }.map{|s| s.keep_if{|k,v| ["article_title","date","journal","pmid"].include?(k)}}
+	end
+end
 
 
-
+if __FILE__ == $0
+	parse_json = SRAsJSONParser.new(ARGV[0])
+	#puts parse_json.all_subid
+	pp parse_json.journal_info("SRA009031")
 end
