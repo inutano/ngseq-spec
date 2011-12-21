2420df4814859823c52c2d29c64bd76e4bc2c06c
iNut <iNut@tkrtng.(none)>
2011-12-21 17:32:17 +0900

    parse_sras_json.rb worked!

---
+++ parse_sras_json.rb
@@ -0,0 +1,14 @@
+#!/home/iNut/.rvm/rubies/ruby-1.9.2-p180/bin/ruby
+# -*- coding: utf-8 -*-
+
+require "json"
+require "open-uri"
+require "pp"
+
+class parse_sras_json
+	def initialize
+		@wholeset = 
+
+
+
+end
