70b4a3e47d82e2841d13577582e5b6da13fdefd3
iNut <iNut@tkrtng.(none)>
2011-12-26 15:46:01 +0900

    still coding operation.rb

--- operation.rb
+++ operation.rb
@@ -18,8 +18,30 @@ def ftpsession
 	end
 end
 
+def checkdumplist
+	dumplist = `ls ./litesra | grep '.lite.sra$'`.split.map{|n| n.gsub(".lite.sra","")}
+	!dumplist.empty?
+end
+
+def checkfqlist
+	fqlist = `ls ./fq | grep '.fastq'`.split.map{|n| n.gsub(".bz2","").gsub(".gz","")}
+	!fqlist.empty?
+end
+
+
+## ignition
 while diskcheck && ftpsession
 	puts "disk usage: #{diskcheck}"
 	puts "number of lftp process: #{ftpsession - 1}"
-	`./lftp.sh #{fileid}`
+	#`./lftp.sh -o ./litesra #{fileid}`
+end
+
+while checkdumplist
+	puts "start fastq-dump for #{fileid}"
+	#`qsub -j y -o log/fqdump_#{Time.now.strftime("%4Y%m%d%H%M%S")}.log fastqdump.sh`
+end
+
+while checkfqlist
+	puts "start fastqc for #{fileid}"
+	#`qsub -j y -o log/fastqc_#{Time.now.strftime("%4Y%m%d%H%M%S")}.log fastqc.sh`
 end
