ab18090b893f5b68d0b8224c5651776d9bae16ed
tazro inutano ohta <inutano@gmail.com>
2012-01-27 16:53:52 +0900

    add class for activerecord, delete useless STDOUT

--- lib/list_update.rb
+++ lib/list_update.rb
@@ -3,6 +3,30 @@
 
 require "#{File::expand_path(File::dirname(__FILE__))}/parse_sras_json.rb"
 require "fileutils"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+	:adapter => "sqlite3",
+	:database => "./production.sqlite3"
+)
+
+class SRAIDsInit < ActiveRecord::Migration
+	def self.up
+		create_table( :sraids ) do |t|
+			t.string :runid, :null => false
+			t.string :status, :null => false # done, ongoing, available, missing
+			t.boolean :paper, :null => false # paper published or not
+			t.timestamps
+		end
+	end
+	
+	def self.down
+		drop_table :sraids
+	end
+end
+
+class SRAID < ActiveRecord::Base
+end
 
 class IDlistUpdate
 	def initialize
@@ -12,33 +36,28 @@ class IDlistUpdate
 	end
 
 	def run_members
-		puts "updating SRA_Run_Members... #{Time.now.strftime("%H:%M:%S")}"
 		FileUtils.mv(@run_members,"./previous/SRA_Run_Members_#{Time.now.strftime("%m%d%H%M%S")}.tab") if File.exist?(@run_members)
 		`lftp -c "#{@com} SRA_Run_Members.tab"`
 		File.open(@run_members).readlines
 	end
 	
 	def accessions
-		puts "updating SRA_Accessions... #{Time.now.strftime("%H:%M:%S")}"
 		FileUtils.mv(@accessions,"./previous/SRA_Accessions_#{Time.now.strftime("%m%d%H%M%S")}.tab") if File.exist?(@accessions)
 		`lftp -c "#{@com} SRA_Accessions.tab"`
 		File.open(@accessions).readlines
 	end
 	
 	def available_runid(run_members)
-		puts "updating available run id... #{Time.now.strftime("%H:%M:%S")}"
 		run_members.delete_if{|l| l !~ /live/ }.map{|l| l.split("\t").first }.uniq
 	end
 	
 	def paperpublished_subid
-		puts "updating paper-published submission id... #{Time.now.strftime("%H:%M:%S")}"
 		FileUtils.mv("./publication.json","./previous/publication_#{Time.now.strftime("%m%d%H%M%S")}.json") if File.exist?("./publication.json")
 		`wget -O ./publication.json "http://sra.dbcls.jp/cgi-bin/publication2.php"`
 		SRAsJSONParser.new("./publication.json").all_subid
 	end
 	
 	def paperpublished_runid(pub_subid, accessions)
-		puts "updating paper published run id... #{Time.now.strftime("%H:%M:%S")}"
 		sub_run = {}
 		accessions.delete_if{|l| !(l =~ /^(S|E|D)RR/ && l.include?("live"))}.each do |l|
 			sub_run[l.split(",").last] ||= []
@@ -48,11 +67,11 @@ class IDlistUpdate
 	end
 	
 	def list4process(pub_runid, available_runid)
-		puts "updating list for processing quality check... #{Time.now.strftime("%H:%M:%S")}"
 		(pub_runid + available_runid).uniq
 	end
-	
 end
+
+
 
 if __FILE__ == $0
 	if ARGV[0] == "--run-update"
