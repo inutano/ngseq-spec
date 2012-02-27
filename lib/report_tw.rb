171da5959543f156abf594e7b506105142dd4c6d
tazro inutano ohta <inutano@gmail.com>
2012-02-27 19:02:52 +0900

    create reporttwitter class in lib/report_tw.rb

---
+++ lib/report_tw.rb
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+require "twitter"
+
+class ReportTwitter
+  @@tw = Twitter::Client.new
+  @@time = Time.now.strftime("%m/%d %H:%M:%S")
+	
+  def self.stat
+    usage = `df -h`.split("\n").select{|l| l =~ /home/ }.map{|l| l.split(/\s+/)}.flatten[4]
+    session = `ps aux`.split("\n").select{|l| l =~ /lftp/ }.length / 2
+    job = `qstat -u iNut`.split("\n").select{|l| l =~ /^[0-9]/}.length
+    message = <<-MESSAGIO.gsub(/^\s*/,"")
+      @null #{@time}
+      disk usage: #{usage}%
+      #{session} ftp sessions
+      #{job} job submitted
+    MESSAGIO
+    @@tw.update(message)
+  end
+	
+  def self.job(all, done, ongoing)
+    message = <<-MESSAGIO.gsub(/^\s*/,"")
+      @null #{@time}
+      #{done.length} of runs finished,
+      #{ongoing.length} of runs in progress.
+      #{(done.length.to_f / all.length) * 100}%
+    MESSAGIO
+    @@tw.update(message)
+  end
+	
+  def self.error(missing)
+    missing.join(",").scan(/.{100}/).each do |list|
+      message = <<-MESSAGIO.gsub(/^\s*/,"")
+        @null #{@time}
+        error occurred:
+        #{list.gsub(/,$/,"")}
+      MESSAGIO
+      @@tw.update(message)
+    end
+  end
+end
