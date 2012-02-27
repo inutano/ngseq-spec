e398ba7526b90b5d3d8794caba272fd08e7a9fa4
tazro inutano ohta <inutano@gmail.com>
2012-02-27 19:51:45 +0900

    fasftqc script for fq ver. fastqc_fq

---
+++ lib/fastqc_fq.sh
@@ -0,0 +1,17 @@
+#!/home/iNut/local/bin/zsh
+#$ -j y
+# fastqc_fq.sh <runid>
+
+project_dir="/home/iNut/project/sra_qualitycheck"
+fastqc_path="/home/iNut/local/bin/fastqc/fastqc"
+
+runid=$1
+data_dir="${project_dir}/data"
+result_dir="${project_dir}/result/${runid}"
+target_file="${project_dir}/${runid}*fastq*"
+
+fastqc_command="${fastqc_path} --noextract --threads 8 --outdir ${result_dir} ${data_dir}/${runid}*fastq*"
+cleaning_command="rm -fr ${data_dir}/${runid}*fastq*"
+
+mkdir ${result_dir}
+${fastqc_command} && ${cleaning_command}
