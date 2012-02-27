61583ea6d6a3d285ca6a1c1916e91246727f697d
tazro inutano ohta <inutano@gmail.com>
2012-02-27 21:00:42 +0900

    fixed variant mistake and simplified

--- lib/report_tw.rb
+++ lib/report_tw.rb
@@ -4,14 +4,13 @@ require "twitter"
 
 class ReportTwitter
   @@tw = Twitter::Client.new
-  @@time = Time.now.strftime("%m/%d %H:%M:%S")
-	
+  
   def self.stat
     usage = `df -h`.split("\n").select{|l| l =~ /home/ }.map{|l| l.split(/\s+/)}.flatten[4]
     session = `ps aux`.split("\n").select{|l| l =~ /lftp/ }.length / 2
     job = `qstat -u iNut`.split("\n").select{|l| l =~ /^[0-9]/}.length
     message = <<-MESSAGIO.gsub(/^\s*/,"")
-      @null #{@time}
+      @null #{Time.now.strftime("%m/%d %H:%M:%S")}
       disk usage: #{usage}%
       #{session} ftp sessions
       #{job} job submitted
@@ -19,11 +18,10 @@ class ReportTwitter
     @@tw.update(message)
   end
 	
-  def self.job(all, done, ongoing)
+  def self.job(done, all)
     message = <<-MESSAGIO.gsub(/^\s*/,"")
-      @null #{@time}
+      @null #{Time.now.strftime("%m/%d %H:%M:%S")}
       #{done.length} of runs finished,
-      #{ongoing.length} of runs in progress.
       #{(done.length.to_f / all.length) * 100}%
     MESSAGIO
     @@tw.update(message)
@@ -32,7 +30,7 @@ class ReportTwitter
   def self.error(missing)
     missing.join(",").scan(/.{100}/).each do |list|
       message = <<-MESSAGIO.gsub(/^\s*/,"")
-        @null #{@time}
+        @null #{Time.now.strftime("%m/%d %H:%M:%S")}
         error occurred:
         #{list.gsub(/,$/,"")}
       MESSAGIO
