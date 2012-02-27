a72c53ffeb60ca325b066998df25bf55b1e3b2f3
tazro inutano ohta <inutano@gmail.com>
2012-02-27 21:02:33 +0900

    simpified lib/sraid.rb

--- lib/sraid.rb
+++ lib/sraid.rb
@@ -7,12 +7,10 @@ class SRAID < ActiveRecord::Base
     "#{runid}, status => #{status}, paper => #{paper}"
   end
   
-  scope :task, where( :status => "available" ).order("paper DESC, runid ASC")
+  scope :available, where( :status => "available" ).order("paper DESC, runid ASC")
   scope :paper_published, where( :status => "available", :paper => true )
   scope :paper_unpublished, where( :status => "available", :paper => false )
-  scope :available, where( :status => "available" )
   scope :done, where( :status => "done" )
-  scope :ongoing, where( :status => "ongoing" )
   scope :downloaded, where( :status => "downloaded" )
   scope :missing, where( :status => "missing" )
   scope :reported, where( :status => "reported" )
