9134a2d70422a9e0568b8cf40f322f4b3123c0a4
tazro inutano ohta <inutano@gmail.com>
2012-03-08 12:46:28 +0900

    refactorred and rewrite list_update.rb to use transaction

--- lib/list_update.rb
+++ lib/list_update.rb
@@ -1,7 +1,6 @@
-#!/usr/env/ruby
 # -*- coding: utf-8 -*-
 
-require "#{File::expand_path(File::dirname(__FILE__))}/parse_sras_json.rb"
+require "#{File.expand_path(File.dirname(__FILE__))}/parse_sras_json.rb"
 require "fileutils"
 require "active_record"
 
@@ -14,10 +13,10 @@ class SRAIDsInit < ActiveRecord::Migration
   def self.up
     create_table( :sraids ) do |t|
       t.string :runid, :null => false
-      t.string :subid
-      t.string :studyid
-      t.string :expid
-      t.string :sampleid
+      t.string :subid, :null => false
+      t.string :studyid, :null => false
+      t.string :expid, :null => false
+      t.string :sampleid, :null => false
       t.string :status, :null => false # done, ongoing, available, missing
       t.boolean :paper, :null => false # paper published or not
       t.timestamps
@@ -31,32 +30,26 @@ class SRAID < ActiveRecord::Base
 end
 
 class Update
-  @@run_members = "./SRA_Run_Members.tab"
   @@accessions = "./SRA_Accessions.tab"
-
+  @@run_members = "./SRA_Run_Members.tab"
+  @@publication = "./publication.json"
+  
+  def self.accessions
+    FileUtils.mv(@@accessions, "./previous/SRA_Accessions_#{Time.now.strftime("%m%d%H%M%S")}.tab") if File.exist?(@@accessions)
+    `lftp -c "open ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Accessions.tab"`
+    File.open(@@accessions).readlines
+  end
+  
   def self.run_members
     FileUtils.mv(@@run_members,"./previous/SRA_Run_Members_#{Time.now.strftime("%m%d%H%M%S")}.tab") if File.exist?(@@run_members)
     `lftp -c "open ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Run_Members.tab"`
     File.open(@@run_members).readlines
   end
-  def self.accessions
-    FileUtils.mv(@@accessions,"./previous/SRA_Accessions_#{Time.now.strftime("%m%d%H%M%S")}.tab") if File.exist?(@@accessions)
-    `lftp -c "open ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Accessions.tab"`
-    File.open(@@accessions).readlines
-  end
-  def self.available_runid(run_members)
-    run_members.select{|l| l =~ /live/ }.map{|l| l.split("\t").first }.uniq
-  end
+
   def self.paperpublished_subid
-    FileUtils.mv("./publication.json","./previous/publication_#{Time.now.strftime("%m%d%H%M%S")}.json") if File.exist?("./publication.json")
+    FileUtils.mv(@@publication, "./previous/publication_#{Time.now.strftime("%m%d%H%M%S")}.json") if File.exist?(@@publication)
     `wget -O ./publication.json "http://sra.dbcls.jp/cgi-bin/publication2.php"`
-    SRAsJSONParser.new("./publication.json").all_subid
-  end
-  def self.paperpublished_runid(pub_subid, accessions)
-    sub_runid = pub_subid.map do |subid|
-      accessions.select{|l| l.include?(subid) && l=~ /^.RR/ && l.include?("live") }.map{|l| l.split("\t").first }
-    end
-    sub_runid.flatten.uniq
+    SRAsJSONParser.new(@@publication).all_subid
   end
 end
 
@@ -64,49 +57,49 @@ if __FILE__ == $0
   # create db file and table if it  does not exist
   SRAIDsInit.migrate( :up ) if !File.exist?("./production.sqlite3")
   
-  puts "updating SRA_Run_Members.tab #{Time.now}"
-  run_members = Update.run_members
-	
-  puts "updating SRA_Accessions.tab #{Time.now}"
-  accessions = Update.accessions
-
-  puts "putting live id to db.. #{Time.now}"
-  Update.available_runid(run_members).each do |id|
-    SRAID.create( :runid => id, :status => "available", :paper => false ) if !SRAID.find_by_runid(id)
+  # preparing list to insert: available on ftp site and not yet recorded on DB
+  available_on_ftp = Update.accessions.select{|l| l =~ /^.RR/ && l.split("\t")[2] == "live"}
+  already_in_db = SRAID.all.map{|r| r.runid }
+  update_list =  available_on_ftp.delete_if{|l| already_in_db.include?(l.split("\t").first)}
+  
+  # for the first time to create DB, devide already done and undone
+  result_dirs = Dir.entries("../result")  
+  done = update_list.select{|l| result_dirs.include?(l.split("\t").first)}
+  undone = update_list - done
+  
+  [done, undone].each do |set|
+    SRAID.transaction {
+      set.each do |line|
+        runid = line[0]
+        subid = line[1]
+        studyid = line[12]
+        expid = line[10]
+        sampleid = line[11]
+        if set == done
+          status = "done"
+        else
+          status = "available"
+        end
+        
+        SRAID.create (
+          :runid => runid,
+          :subid => subid,
+          :studyid => studyid,
+          :expid => expid,
+          :sampleid => sampleid,
+          :status => status,
+          :paper => false
+        )
+      end
+    }
   end
-	
-  puts "updating publication.json from DBCLS SRAs #{Time.now}"
-  pub_subid = Update.paperpublished_subid
-	
-  puts "mark paper-publushed run id on db #{Time.now}"
-  Update.paperpublished_runid(pub_subid, accessions).each do |id|
-    record = SRAID.find_by_runid(id)
-    if record
+  
+  # update publication info
+  SRAID.transaction {
+    Update.paperpublished_subid.each do |subid|
+      record = SRAID.find_by_subid(subid)
       record.paper = true
       record.save
     end
-  end
-  
-  puts "search submission, project, experiment ID and put them into DB #{Time.now}"
-  SRAID.all.each do |record|
-    runid = record.runid
-    acc = accessions.select{|line| line =~ /^#{runid}/ }.join.split("\t")
-    record.subid = acc[1]
-    record.studyid = acc[12]
-    record.expid = acc[10]
-    record.sampleid = acc[11]
-    record.save
-    puts "record for #{record.runid}: #{record.subid}, #{record.studyid}, #{record.expid}, #{record.sampleid}"
-  end
-  
-  puts "mark already calcurated items as done #{Time.now}"
-  SRAID.all.each do |record|
-    runid = record.runid
-    result_dir = "../result/#{runid}"
-    if record.status != "done" && record.status != "ongoing" && File.exist?(result_dir)
-      record.status = "done"
-      record.save
-    end
-  end  
+  }
 end
-
