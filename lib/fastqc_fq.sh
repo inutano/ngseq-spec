fb410a2a42ed6848227dfd759135e2ee4433e35b
tazro inutano ohta <inutano@gmail.com>
2012-04-04 15:42:38 +0900

    imported from Rina Aizawa ver. and fixed minor bugs

--- lib/fastqc_fq.sh
+++ lib/fastqc_fq.sh
@@ -1,8 +1,9 @@
-#$ -S /bin/bash -j y
+#!/home/iNut/local/bin/zsh
+#$ -j y
 # fastqc_fq.sh <runid>
 
-project_dir="/home/inutano/project/sra_qualitycheck"
-fastqc_path="/home/inutano/local/bin/fastqc/fastqc"
+project_dir="/home/iNut/project/sra_qualitycheck"
+fastqc_path="/home/iNut/local/bin/fastqc/fastqc"
 
 runid=$1
 data_dir="${project_dir}/data"
@@ -10,7 +11,7 @@ result_dir="${project_dir}/result/${runid}"
 
 fastqc_command="${fastqc_path} --noextract --outdir ${result_dir} ${data_dir}/${runid}*fastq*"
 cleaning_command="rm -fr ${data_dir}/${runid}*fastq*"
-gunzip="/usr/local/bin/gunzip"
+gunzip="/bin/gunzip"
 bunzip="/usr/bin/bunzip2"
 
 if [ ! -e ${result_dir} ] ; then
@@ -21,12 +22,12 @@ files=(`ls ${data_dir}/${runid}*`)
 
 case "${files[0]}" in
   *.tar.gz )
-    /bin/tar zxfv ${files[@]} && ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
+    /bin/tar zxfv ${files[@]} -C ${data_dir} && ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
   *.tar.bz2 )
-    /bin/tar jxfv ${files[@]} && ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
+    /bin/tar jxfv ${files[@]} -C ${data_dir} && ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
   *.bz2 )
-    /usr/bin/bunzip2 ${files[@]} && ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
-  *.gz2 )
+    cd ${data_dir} && /usr/bin/bunzip2 ${files[@]} && ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
+  *.gz )
     ${fastqc_command} && ${cleaning_command} || (echo "failed fastqc" && ${cleaning_command}) ;;
   * )
     echo "failed: unknown file type" && exit 1 ;;
