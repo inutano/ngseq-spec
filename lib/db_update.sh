edf02e4c6a23f7a1c2f8869da8ad69aa87af3d41
tazro inutano ohta <inutano@gmail.com>
2013-01-18 18:15:52 +0900

    shell script for running on rina-chan

---
+++ lib/db_update.sh
@@ -0,0 +1,5 @@
+#$ -S /home/inutano/local/bin/zsh -j y -l mem_req=2G,s_vmem=2G -pe def_slot 16
+
+export PATH="/home/inutano/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
+(/home/inutano/.rbenv/shims/ruby /home/inutano/project/sra_qualitycheck_v2/lib/db_update.rb --update --manual 2>&1) >> /home/inutano/project/sra_qualitycheck_v2/log/db.log
