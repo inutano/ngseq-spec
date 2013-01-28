3b26a3b3bf35de61a6c20aba9c306451dc51249c
tazro inutano ohta <inutano@gmail.com>
2013-01-28 16:36:21 +0900

    added unarchive&fastqc section for *.sra ; wondering if this change works :(

--- lib/fastqc.sh
+++ lib/fastqc.sh
@@ -15,6 +15,8 @@ result_dir="${result_path}/${runid_head}/${runid}"
 result_file=`echo ${fpath} | sed -e 's:^.\+/::g' | sed -e 's:\..\+$:_fastqc.zip:g'`
 result_fpath="${result_dir}/${result_file}"
 
+dump=`grep "^fastq-dump_path" ${config_path} | cut -d '"' -f 2`
+
 if [ -e ${result_fpath} ] ; then
   rm -f ${result_fpath}
 fi
@@ -51,6 +53,26 @@ case "${fpath}" in
     fastqc="${fastqc_path} --noextract --outdir ${result_dir} ${fpath}"
     cleaning="rm -fr ${fpath}"
     ${fastqc} && ${cleaning} || touch "${fpath}_failed" ;;
+
+  *.sra )
+    unarchived=`echo ${fpath} | sed -e 's:\.sra$::' | sed -e 's:\.lite$::' | sed -e 's:$:.fastq:'`
+    fastqc="${fastqc_path} --noextract --outdir ${result_dir} ${unarchived}"
+    cleaning="rm -fr ${unarchived}"
+    cd ${data_path} && ${dump} ${fpath} && ${fastqc} && ${cleaning} || touch "${unarchived}_failed"
+
+    unarchived_1=`echo ${fpath} | sed -e 's:\.sra$::' | sed -e 's:\.lite$::' | sed -e 's:$:_1.fastq:'`
+    if [ -e ${unarchived_1} ] ; then
+      fastqc_1="${fastqc_path} --noextract --outdir ${result_dir} ${unarchived_1}"
+      cleaning_1="rm -fr ${unarchived_1}"
+      cd ${data_path} && ${fastqc_1} && ${cleaning_1} || touch "${unarchived_1}_failed"
+    fi
+
+    unarchived_2=`echo ${fpath} | sed -e 's:\.sra$::' | sed -e 's:\.lite$::' | sed -e 's:$:.fastq_2:'`
+    if [ -e ${unarchived_2} ] ; then
+      fastqc_2="${fastqc_path} --noextract --outdir ${result_dir} ${unarchived_2}"
+      cleaning_2="rm -fr ${unarchived_2}"
+      cd ${data_path} && ${fastqc_2} && ${cleaning_2} || touch "${unarchived_2}_failed"
+    fi ;;
 
   * )
     echo "failed: unknown file type" && exit 1 ;;
