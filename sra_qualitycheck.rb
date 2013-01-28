8617d93a20174074d97ee5e63a72c716f3b9127b
tazro inutano ohta <inutano@gmail.com>
2013-01-28 15:52:32 +0900

    defined --transfer-sra section

--- sra_qualitycheck.rb
+++ sra_qualitycheck.rb
@@ -14,6 +14,7 @@ end
 
 class Filecheck
   @@base_path = "/usr/local/ftp/ddbj_database/dra/fastq"
+  @@base_sra_path = "/usr/local/ftp/ddbj_database/dra/sralite/ByExp/litesra"
 
   def initialize(record)
     @runid = record.key.key
@@ -26,6 +27,19 @@ class Filecheck
   def files
     if File.exist?(@fpath)
       flist = Dir.entries(@fpath).select{|f| f =~ /#{@runid}/ }
+      flist if !flist.empty?
+    end
+  end
+  
+  def fpath_sra
+    db_head = @expid.slice(0..2)
+    exp_head = @expid.slice(0..5)
+    File.join(@@base_sra_path, db_head, exp_head, @expid, @runid)
+  end
+  
+  def files_sra
+    if File.exist?(self.fpath_sra)
+      flist = Dir.entries(self.fpath_sra).select{|f| f =~ /#{@runid}/ }
       flist if !flist.empty?
     end
   end
@@ -70,12 +84,6 @@ def running_fastqc(runid, fpath, config_path)
   `#{qsub} -N #{runid} -o #{log} #{lib_path}/fastqc.sh #{runid} #{fpath} #{config_path}`
 end
 
-def capacity  
-end
-
-def job_limit
-end
-
 if __FILE__ == $0
   config_path = File.join(File.expand_path(File.dirname(__FILE__)), "lib", "config.yaml")
   config = YAML.load_file(config_path)
@@ -87,8 +95,7 @@ if __FILE__ == $0
   when "--transfer"
     db = Groonga["SRAIDs"]
     
-    #loop do
-    20.times do
+    loop do
       mess "begin transmission"
       available = db.select{|r| r.status == 1 }.map{|r| r }
       to_be_processed = available[0..15]
@@ -126,6 +133,61 @@ if __FILE__ == $0
         if files_downloaded.select{|f| f =~ /#{id}/ }.empty?
           # failed to download => missing
           hash[:record].status = 4
+          mess "download failed: #{hash[:record].key.key} at #{hash[:fpath]}"
+        else
+          # downloaded
+          hash[:record].status = 3
+          mess "success! #{hash[:record].key.key}"
+        end
+      end
+      
+      Ptransfer.flush
+      
+      mess "sleep 10sec"
+      sleep 10
+    end
+
+  when "--transfer-sra"
+    db = Groonga["SRAIDs"]
+    
+    loop do
+      mess "begin transmission"
+      missing = db.select{|r| r.status == 4 }.map{|r| r }
+      to_be_processed = missing[0..15]
+      
+      if to_be_processed.empty?
+        mess "all missing entries calcurated!"
+        exit
+      end
+      
+      file_status = to_be_processed.map do |record|
+        fc = Filecheck.new(record)
+        files = fc.files_sra
+        fpath = fc.fpath_sra
+        { record: record, files: files, fpath: fpath }
+      end
+
+      file_notfound = file_status.select{|h| !h[:files] }
+      file_notfound.each do |hash|
+        # file not found again => lost 7
+        record = hash[:record]
+        record.status = 7
+        mess "lite.sra file not found: #{hash[:record].key.key} at #{hash[:fpath]}"
+      end
+      
+      file_exist = file_status.select{|h| h[:files] }
+      
+      Ptransfer.load_files(config_path)
+      Ptransfer.each(file_exist)
+      
+      download_path = config["download_path"]
+      files_downloaded = Dir.entries(download_path)
+      
+      file_exist.each do |hash|
+        id = hash[:record].key.key
+        if files_downloaded.select{|f| f =~ /#{id}/ }.empty?
+          # failed to download => lost 7
+          hash[:record].status = 7
           mess "download failed: #{hash[:record].key.key} at #{hash[:fpath]}"
         else
           # downloaded
@@ -184,13 +246,21 @@ if __FILE__ == $0
     
     #array = (367..371).to_a.map{|n| "DRR" + "%06d" % n }
     #array = open("./list").readlines
-    array = db.select{|rec| rec.status == 5 }
+    #array = open("./error_list").readlines
     #array = open("./list2").readlines
+    #array = db.select{|rec| rec.status == 5 }
+    #array = Dir.glob("./data/*.fastq.bz2")
+    array = []
     array.each do |node|
+      #id = node.gsub(/^\.\/data\//,"").gsub(/\.fastq\.bz2$/,"").gsub(/_.$/,"") 
+      #record = db[id]
       #record = db[node.chomp]
       #record.status = 3
-      ap node.key.key
-      node.status = 1
+      #record.status = 1
+      #record.status = 6
+      #ap node.key.key
+      #node.status = 6
+      #ap record.status
     end
     
     ap "available: " + db.select{|r| r.status == 1 }.size.to_s
@@ -199,6 +269,7 @@ if __FILE__ == $0
     ap "missing: " + db.select{|r| r.status == 4 }.size.to_s
     ap "processing: " + db.select{|r| r.status == 5 }.size.to_s
     ap "done: " + db.select{|r| r.status == 6 }.size.to_s
+    ap "lost: " + db.select{|r| r.status == 7 }.size.to_s
   end
 end
  
