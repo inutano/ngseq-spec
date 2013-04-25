36487c1f9ccc530425be7d21729f2a3d2aa56ac9
tazro inutano ohta <inutano@gmail.com>
2013-04-25 09:06:42 +0900

    changed directory data to fastq

--- simple_fastqc.sh
+++ simple_fastqc.sh
@@ -1,5 +1,5 @@
 #$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=16G,s_vmem=16G -pe def_slot 1
-# ls data/ | sed -e 's:data/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
+# ls fastq/ | sed -e 's:fastq/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
 
-cd /home/inutano/project/ER/data
-/home/inutano/local/bin/fastqc --noextract $1
+cd /home/inutano/project/ER/fastq
+/home/inutano/local/bin/fastqc --noextract $1 && rm -f $1
