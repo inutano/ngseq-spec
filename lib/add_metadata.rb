99267e52a3b444ee003fd80d6bad60cbf92e0054
tazro inutano ohta <inutano@gmail.com>
2013-06-10 18:02:37 +0900

    fully refactorred metadata addition, not using external object producing

--- lib/add_metadata.rb
+++ lib/add_metadata.rb
@@ -1,6 +1,28 @@
 require "groonga"
 require "parallel"
-require "./lib_db_update"
+require "nokogiri"
+
+def metadata_extract(acc, exp)
+  base = "/Users/inutano/project/soylatte/data/sra_metadata"
+  xml = File.join(base, acc.slice(0..5), acc, acc + ".experiment.xml")
+  
+  nkgr = Nokogiri::XML(open(xml)).css("EXPERIMENT").select{|n| n.attr("accession") == exp }.first
+  raise NameError, "experiment id not found" unless nkgr
+
+  inst = nkgr.css("INSTRUMENT_MODEL").inner_text
+  platform = inst.split("\s").first if inst
+  [ inst,
+    platform,
+    nkgr.css("LIBRARY_STRATEGY").inner_text,
+    nkgr.css("LIBRARY_SOURCE").inner_text,
+    nkgr.css("LIBRARY_SELECTION").inner_text,
+    nkgr.css("LIBRARY_LAYOUT").first.children[1].name,
+    nkgr.css("LIBRARY_LAYOUT").first.children[1].attr("NOMINAL_LENGTH").to_s,
+    nkgr.css("LIBRARY_LAYOUT").first.children[1].attr("NOMINAL_SDEV").to_s,
+  ]
+rescue NameError, Errno::ENOENT
+  []
+end
 
 if __FILE__ == $0
   input_file = ARGV.first
@@ -19,7 +41,7 @@ if __FILE__ == $0
                  "genus",
                ]
   puts header.chomp + "\t" + add_header.join("\t")
-  
+
   db_path = "../db/project.db"
   Groonga::Database.open(db_path)
   runs = Groonga["Runs"]
@@ -51,25 +73,24 @@ if __FILE__ == $0
     end
   end
   
-  DBupdate.load_file("./config.yaml")
-  while !not_recorded.empty?
-    to_be_processed = not_recorded.shift(24)
-    added_meta = Parallel.map(to_be_processed) do |line|
-      id = line.slice(0..8)
-      i = DBupdate.new(id).run_insert
-      inst = i[:instrument]
-      platform = inst.split("\s").first if inst
-      a = [ inst,
-            platform,
-            i[:library_strategy],
-            i[:library_source],
-            i[:library_selection],
-            i[:library_layout],
-            i[:library_nominal_length],
-            i[:library_nominal_sdev],
-          ]
-      line + "\t" + a.join("\t")
-    end
-    puts added_meta
+  acc_hash = {} # run id => submissionid
+  acc_raw = `awk -F '\t' '$1 ~ /^.RR/ { print $1 "\t" $2 }' "../sra_metadata/SRA_Accessions"`
+  acc_raw.split("\n").each do |line|
+    id_acc = line.split("\t")
+    acc_hash[id_acc[0]] = id_acc[1]
+  end
+  
+  exp_hash = {} # runid => expid
+  run_raw = `awk -F '\t' '$1 ~ /^.RR/ { print $1 "\t" $3 }' "../sra_metadata/SRA_Run_Members"`
+  run_raw.split("\n").each do |line|
+    id_acc = line.split("\t")
+    exp_hash[id_acc[0]] = id_acc[1]
+  end
+  
+  line_remain =  not_recorded.delete_if{|l| l == "" }
+  line_remain.each do |line|
+    id = line.slice(0..8)
+    a = metadata_extract(acc_hash[id], exp_hash[id])
+    puts line + "\t" + a.join("\t")
   end
 end
