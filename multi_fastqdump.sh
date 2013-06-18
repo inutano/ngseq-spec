5bf182bac12da5dcf1b7aebe4b91ecce3a9c2948
tazro inutano ohta <inutano@gmail.com>
2013-06-18 16:11:34 +0900

    fqdump sucks

---
+++ multi_fastqdump.sh
@@ -0,0 +1,10 @@
+#$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=4G,s_vmem=4G -pe def_slot 1
+# qsub -N put_my_job_in multi_fastqdump.sh
+
+base="/home/inutano/project/ER/data"
+cd ${base}
+ls -tr ${base}/*sra | while read f ; do
+  /home/inutano/local/bin/sratoolkit/fastq-dump --split-3 $f && rm -f $f || mv ${f} /home/inutano/project/ER/fqdumpfailed
+  mv ${base}/${1:0:9}*.fastq /home/inutano/project/ER/fastq
+  echo "${f} finished at `date "+%H:%M:%S"`"
+done
