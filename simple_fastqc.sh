547ada489a53b1b2679a164d85d0d84588bad527
tazro inutano ohta <inutano@gmail.com>
2013-06-18 16:10:05 +0900

    threading specification

--- simple_fastqc.sh
+++ simple_fastqc.sh
@@ -1,5 +1,5 @@
-#$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=4G,s_vmem=4G -pe def_slot 1
+#$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=2G,s_vmem=2G -pe def_slot 4
 # ls fastq/ | sed -e 's:fastq/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
 
 cd /home/inutano/project/ER/fastq
-/home/inutano/local/bin/fastqc --noextract $1 && rm -f $1
+/home/inutano/local/bin/fastqc --thread 4 --noextract $1 && rm -f $1
