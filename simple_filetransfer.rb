2b2c5e3d533392380e1c488187491d4469400abd
tazro inutano ohta <inutano@gmail.com>
2013-06-18 18:26:06 +0900

    fully refactorred

--- simple_filetransfer.rb
+++ simple_filetransfer.rb
@@ -3,65 +3,76 @@
 require "parallel"
 require "fileutils"
 
+BASE = "/home/inutano"
+ACC = BASE + "/project/ER/table/SRA_Accessions.tab"
+
+def get_fq(id)
+  fq_awk = `awk -F '\t' '$1 ~ /^#{id}/ { printf "/" "%.6s" "/" $2 "/" $11, $2 }' #{ACC}`
+  fq_path = BASE + "/data/fastq_data" + fq_awk
+  if File.exist?(fq_path)
+    fq = Dir.entries(fq_path).select{|n| n =~ /^#{id}/ }
+    if !fq.empty?
+      fq.map{|f| File.join(fq_path, f)}
+    end
+  end
+end
+
+def get_sra(id)
+  sra_awk = `awk -F '\t' '$1 ~ /^#{id}/ { printf "/" "%.3s" "/" "%.6s" "/" $11 "/" $1, $11, $11 }' #{ACC}`
+  sralite_path = BASE + "/data/litesra_data/ByExp/litesra" + sra_awk
+  if File.exist?(sralite_path)
+    sralite = Dir.entries(sralite_path).select{|n| n =~ /^#{id}/ }
+    if !sralite.empty?
+      sralite.map{|f| File.join(sralite_path, f)}
+    end
+  end
+end
+
+def conf_fpath(id)
+  files = get_fq(id)
+  if !files
+    files = get_sra(id)
+  end
+  files
+end
+
+def disk_full?
+  data_usage = `du /home/inutano/project/ER/data | cut -f 1`.chomp.to_i
+  fastq_usage = `du /home/inutano/project/ER/fastq | cut -f 1`.chomp.to_i
+  disk_usage = data_usage + fastq_usage
+  if data_usage > 20_000_000_000 or disk_usage > 40_000_000_000
+    true
+  end
+end
+
 if __FILE__ == $0
-  BASE = "/home/inutano"
-  accessions = BASE + "/project/ER/table/SRA_Accessions.tab"
-  run_members = BASE + "/project/ER/table/SRA_Run_Members.tab"
-  fq_base = BASE + "/data/fastq_data"
-  sralite_base = BASE + "/data/litesra_data/ByExp/litesra"
-  
   download_dir = BASE + "/project/ER/download"
   download_log = BASE + "/project/ER/table/download_log"
   download_notfound = BASE + "/project/ER/table/download_notfound"
   data_dir = BASE + "/project/ER/data"
-  
+
   filelist_path = ARGV.first || BASE + "/project/ER/table/filelist"
   filelist = open(filelist_path).readlines
   
   progress = 0
   while !filelist.empty?
-    filepath = Parallel.map(filelist.shift(25)) do |id_n|
-      id = id_n.chomp
-      fq_path = fq_base + `awk -F '\t' '$1 ~ /^#{id}/ { printf "/" "%.6s" "/" $2 "/" $11, $2 }' #{accessions}`
-      sralite_path = sralite_base + `awk -F '\t' '$1 ~ /^#{id}/ { printf "/" "%.3s" "/" "%.6s" "/" $11 "/" $1, $11, $11 }' #{accessions}`
-      [id, fq_path, sralite_path]
-    end
-    
     download = []
     no_file = []
-    
-    filepath.each do |fpath_a|
-      id = fpath_a[0]
-      fq_path = fpath_a[1]
-      sralite_path = fpath_a[2]
-      
-      if File.exist?(fq_path)
-        fq = Dir.entries(fq_path).select{|n| n =~ /^#{id}/ }
-        if !fq.empty?
-          download << fq.map{|f| File.join(fq_path, f) }
-        else
-          if File.exist?(sralite_path)
-            sralite = Dir.entries(sralite_path).select{|n| n =~ /^#{id}/ }
-            if !sralite.empty?
-              download << sralite.map{|f| File.join(sralite_path, f) }
-            else
-              no_file << id
-            end
-          else
-            no_file << id
-          end
-        end
-      elsif File.exist?(sralite_path)
-        sralite = Dir.entries(sralite_path).select{|n| n =~ /^#{id}/ }
-        if !sralite.empty?
-          download << sralite.map{|f| File.join(sralite_path, f) }
-        else
-          no_file << id
-        end
+    filelist.shift(25).each do |id_n|
+      id = id_n.chomp
+      fa = conf_fpath(id)
+      if fa
+        download << fa
       else
         no_file << id
       end
     end
+    
+    #test
+    require "ap"
+    ap download
+    ap no_file
+    exit
     
     threads = []
     download.flatten.each do |file|
@@ -74,14 +85,12 @@ if __FILE__ == $0
       threads << th
     end
     threads.each{|th| th.join }
+    
     open(download_notfound,"a"){|f| f.puts(no_file) }
     progress += 25
     puts "#{Time.now}\t" + progress.to_s + " files transferred"
     
-    data_usage = `du /home/inutano/project/ER/data | cut -f 1`.chomp.to_i
-    fastq_usage = `du /home/inutano/project/ER/fastq | cut -f 1`.chomp.to_i
-    disk_usage = data_usage + fastq_usage
-    if data_usage > 20_000_000_000 or disk_usage > 40_000_000_000
+    if disk_full?
       puts "disk quota nearly exceeded. Type 'continue' to restart"
       str = ""
       while str != "continue"
