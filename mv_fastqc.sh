db33dd637aaf97a3c8776f7625a5c4856e92aa92
tazro inutano ohta <inutano@gmail.com>
2013-06-18 16:09:41 +0900

    remove old file

--- mv_fastqc.sh
+++ mv_fastqc.sh
@@ -4,9 +4,13 @@ cd ./fastq
 ls |\
 awk '$1 ~ /fastqc.zip$/ { print $1 }' |\
 while read f ; do
-  dir=`echo $f | awk '{ printf "/home/inutano/backup/fastqc_result/" "%.6s" "/" "%.9s", $1, $1 }'`
-  if [ ! -e $dir ] ; then
-    mkdir -p $dir
+  dir=`echo ${f} | awk '{ printf "/home/inutano/backup/fastqc_result/" "%.6s" "/" "%.9s", $1, $1 }'`
+  if [ ! -e ${dir} ] ; then
+    mkdir -p ${dir}
   fi
-  mv $f $dir
+  old="${dir}/${f}"
+  if [ -e ${old} ] ; then
+    rm -f ${old}
+  fi
+  mv ${f} "${dir}/"
 done
