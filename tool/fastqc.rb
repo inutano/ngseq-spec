70949b164887e54c92346e089580066a5bdec707
tazro inutano ohta <inutano@gmail.com>
2013-10-18 21:03:21 +0900

    automation yeah

---
+++ tool/fastqc.rb
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+require "rake"
+require "fileutils"
+
+Basedir = "/home/inutano/project/ER"
+
+def fastq_order_by_size
+  fastq_dir = Basedir + "/fastq"
+  fq_files = Dir.glob(fastq_dir + "/*fastq")
+  fq_files.sort_by{|f| File.size(f) }
+end
+
+def qsub_fastqc(fastq)
+  job_name = fastq.split("/").last.slice(0..9) + "F"
+  script_path = Basedir + "/tool/fastqc.sh"
+  qsub = "/home/geadmin/UGER/bin/lx-amd64/qsub -N #{job_name} #{script_path} #{fastq}"
+  sh qsub
+  job_name
+rescue RuntimeError
+  puts "------ qsub command caused an error for #{fastq} " + Time.now.to_s
+  exit
+end
+
+def job_finished?(job_name)
+  stat = `/home/geadmin/UGER/bin/lx-amd64/qstat | awk '$1 ~ /^[0-9]/ { print $3 }'`
+  !stat.split("\n").include?(job_name)
+end
+
+def disk_full?
+  data_usage = `du /home/inutano/project/ER/data | cut -f 1`.chomp.to_i
+  fastq_usage = `du /home/inutano/project/ER/fastq | cut -f 1`.chomp.to_i
+  disk_usage = data_usage + fastq_usage
+  if fastq_usage > 20_000_000_000 or disk_usage > 40_000_000_000
+    true
+  end
+end
+
+if __FILE__ == $0
+  while true do
+    # anytime disk full: fastqc only reduces the size
+    #if disk_full?
+    #  puts "Disk quota nearly exceeded: sleep until anyone is out " + Time.now.to_s
+    #  while disk_full?
+    #    sleep 10
+    #  end
+    #end
+
+    fastq_list = fastq_order_by_size
+
+    # no file to dump
+    if fastq_list.empty?
+      puts "No file to be processed: sleep until new guys are coming " + Time.now.to_s
+      while fastq_list.empty?
+        sleep 10
+      end
+    end
+    
+    # job submission
+    job_box = []
+    fastq_list.each do |fastq|
+      job_box << qsub_fastqc(fastq)
+    end
+    
+    # waiting for submitted job to finish
+    job_box.each do |job_name|
+      while !job_finished?(job_name)
+        sleep 10
+      end
+    end
+  end
+end
