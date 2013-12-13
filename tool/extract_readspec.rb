67d2a96e544dad89e51919a9c45e765f6759cfab
tazro inutano ohta <inutano@gmail.com>
2013-12-13 14:42:28 +0900

    create an utility module for file manage and added the qc_data extraction part

--- tool/extract_readspec.rb
+++ tool/extract_readspec.rb
@@ -3,26 +3,53 @@
 require "parallel"
 require "./fastqc_result_parser"
 
-QCDir = "../fastqc_data"
-
-def get_qc_files
-  fname = "/fastqc_data.txt"
-  dirs = Dir.glob(QCDir + "/?RR*/?RR*/?RR*_fastqc").map{|p| p + fname }
-  dirs.group_by{|fp| fp.match(/(.RR\d+)_.+qc/)[1] }
+module ReadSpecUtils
+  def self.get_data_path(qc_dir)
+    fname = "/fastqc_data.txt"
+    dirs = Dir.glob(qc_dir + "/?RR*/?RR*/?RR*_fastqc").map{|p| p + fname }
+    dirs.group_by{|fp| fp.match(/(.RR\d+)_.+qc/)[1] }
+  end
 end
 
 class ReadSpec
-  def initialize(run_id)
-    @run_id = run_id
+  def initialize(run_id, read_path_set)
+    @id = run_id
+    @set = read_path_set
   end
   
-  def fastqc_data
+  def get_spec
+    qc_data = get_qc_data
+  end
+  
+  def get_qc_data
+    case @set.size
+    when 1
+      parse_qc_data(@set.first) + ["single"]
+    else
+      merge_qc_data
+    end
+  end
+  
+  def merge_qc_data
+    read1, read2 = [/_1/, /_2/].map{|rex| @set.select{|name| name =~ rex }.first }
+    qc1, qc2 = [read1, read2].map{|read| parse_qc_data(read) }
+    raise NameError if qc1[0] != qc2[0]
+    qc1[0] + (1..8).map{|n| (qc1[n] + qc2[n]) / 2.0 } + ["paired"]
+  end
+  
+  def parse_qc_data(path)
+    p = FastQCParser.new(path)
+    [ p.total_sequences,
+      p.min_length,
+      p.max_length,
+      p.mean_sequence_length,
+      p.median_sequence_length,
+      p.percent_gc,
+      p.normalized_phred_score,
+      p.total_n_content,
+      p.total_duplicate_percentage ]
   end
   
   def metadata
   end
-end
-
-if __FILE__ == $0
-  qc_files = get_qc_files
 end
