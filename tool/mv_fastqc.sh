1be88f6dc5a9c049041ceb7c5865cbf702860c42
tazro inutano ohta <inutano@gmail.com>
2014-02-06 20:06:00 +0900

    fixation for 7-digit IDs

--- tool/mv_fastqc.sh
+++ tool/mv_fastqc.sh
@@ -4,7 +4,9 @@ cd /home/inutano/project/ER/fastq
 ls |\
 awk '$1 ~ /fastqc.zip$/ { print $1 }' |\
 while read f ; do
-  dir=`echo ${f} | awk '{ printf "/home/inutano/backup/fastqc_result/" "%.6s" "/" "%.9s", $1, $1 }'`
+  id=`echo ${f} | sed -e 's:_fastqc.zip::' | sed -e 's:_.::'`
+  idx=`echo ${id} | sed -e 's:...$::'`
+  dir=`echo ${id} | awk -v idx="${idx}" '{ printf "/home/inutano/backup/fastqc_result/" idx "/" $1 }'`
   if [ ! -e ${dir} ] ; then
     mkdir -p ${dir}
   fi
