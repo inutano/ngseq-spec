37710263ceb26ffdb91dc05b8f8432c80677112e
tazro inutano ohta <inutano@gmail.com>
2014-03-04 17:51:49 +0900

    whole refactoring, calculating throughput and coverage, then filtering invalid lines

--- lib/unite_spec.rb
+++ lib/unite_spec.rb
@@ -4,23 +4,86 @@ require "json"
 require "parallel"
 require "fileutils"
 
+def get_throughput(num_of_reads, mean_length, layout)
+  multiple = case layout
+             when "single"
+               1
+             when "paired"
+               2
+             end
+  num_of_reads * mean_length * multiple
+end
+
+def get_coverage(throughput, gsize_mb)
+  gsize = gsize_mb.to_f * 1_000_000
+  if gsize > 0
+    # gsize given by Mb order
+    throughput / gsize
+  else
+    "NA"
+  end
+end
+
+def merge_spec(readspec, md)
+  rm_runid = md.shift
+
+  num_of_reads, mean_length, layout = [1,4,10].map do |n|
+    readspec[n]
+  end
+  gsize = md[15]
+    
+  th = get_throughput(num_of_reads, mean_length, layout)
+  cov = get_coverage(th, gsize)
+  readspec + md + [th, cov]
+end
+
+def line_valid?(a)
+  if a.size == 30
+    tf = (1..9).map do |n|
+      a[n] >= 0
+    end
+    !tf.include?(false)
+  end
+end
+
 if __FILE__ == $0
-  rs_fpath = "../result/readspec.json"
-  ss_fpath = "../result/sequencespec.json"
+  # paths
+  result_path = "../result"
+  rs_fpath = "#{result_path}/readspec.json"
+  ss_fpath = "#{result_path}/sequencespec.json"
   
-  readspec = open(rs_fpath){|f| JSON.parse(f.read, :symbolize_names => true) }
-  sequencespec = open(ss_fpath){|f| JSON.parse(f.read, :symbolize_names => true) }
+  # load json files
+  readspec, sequencespec = [rs_fpath, ss_fpath].map do |fpath|
+    open(fpath){|f| JSON.parse(f.read, :symbolize_names => true) }
+  end
   
+  # threading option
   num_of_threads = 12
-  spec = Parallel.map(readspec, :in_threads => num_of_threads) do |id, readspec_a|
-    md = sequencespec[id].flatten
-    rm_runid = md.shift
-    (readspec_a.flatten + md).join("\t")
-  end
   
-  us_fpath = "../result/unitespec.tab"
-  if File.exist?(us_fpath)
-    FileUtils.mv us_fpath, us_fpath + "." + Time.now.strftime("%Y%m%d-%H%M")
+  # merge readspec and metadata
+  invalid_pairs = []
+  spec = Parallel.map(readspec.first(10000), :in_threads => num_of_threads) do |id, readspec_a|
+    readspec = readspec_a.flatten
+    md = sequencespec[id].flatten
+    
+    line = if readspec[1] != "illegal-pair"
+             merge_spec(readspec, md)
+           else
+             invalid_pairs << id
+             nil
+           end
+    
+    line.join("\t") if line && line_valid?(line)
   end
-  open(us_fpath, "w"){|f| f.puts(spec) }
+    
+  # save results
+  us_fpath = "#{result_path}/unitespec.tab"
+  invalid_fpath = "#{result_path}/invalid_pairs.txt"
+  
+  ts = Time.now.strftime("%Y%m%d-%H%M")
+  [[us_fpath, spec], [invalid_fpath, invalid_pairs]].each do |array|
+    fpath = array[0]
+    FileUtils.mv fpath, fpath + "." + ts if File.exist?(fpath)
+    open(fpath,"w"){|f| f.puts(array[1].compact) }
+  end
 end
